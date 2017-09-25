fe76e4ced3fc4e758ee9c83d35fdbcf75bc8cefd
Aaron <FlashBanistan66@gmail.com>
2017-09-25 17:45:50 -0600

    redirect to home screen

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppWrapperComponent } from './routes/app-wrapper/app-wrapper.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'connect', pathMatch: 'full' },
+  { path: '', redirectTo: 'connect', pathMatch: 'full' },
   { path: 'login', loadChildren: './routes/login/login.module#LoginModule' },
   { path: 'connect', component: AppWrapperComponent, children: [
     { path: 'home', loadChildren: './routes/home/home.module#HomeModule' },
