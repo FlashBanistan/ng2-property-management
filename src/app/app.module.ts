f6726b41d2ab5ae045fa9e5d67747ef0a8add6ca
Aaron <FlashBanistan66@gmail.com>
2017-09-24 20:26:22 -0600

    add clarity for UX/UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { ClarityModule } from "clarity-angular";
 import { AppRouter } from './app-router.module';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ClarityModule.forRoot(),
     AppRouter,
   ],
   exports: [
