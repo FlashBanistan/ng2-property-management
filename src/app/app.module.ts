175fca714bb2af14a5b54880ef1f1bb8d73d922a
Aaron <FlashBanistan66@gmail.com>
2017-09-25 17:46:14 -0600

    provide charge, payment service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpModule } from '@angular/http';
 import { ClarityModule } from "clarity-angular";
 import { AppRouter } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { AppWrapperComponent } from './routes/app-wrapper/app-wrapper.component';
+
+import { PaymentService } from './payments/payment.service';
+import { ChargeService } from './charges/charge.service';
 
 @NgModule({
   declarations: [
@@ -14,13 +18,16 @@ import { AppWrapperComponent } from './routes/app-wrapper/app-wrapper.component'
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     ClarityModule.forRoot(),
     AppRouter,
   ],
   exports: [
   ],
   providers: [
-    { provide: LocationStrategy, useClass: HashLocationStrategy}
+    { provide: LocationStrategy, useClass: HashLocationStrategy},
+    PaymentService,
+    ChargeService
   ],
   bootstrap: [AppComponent]
 })
