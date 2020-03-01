cc2e6cdd1b849eb140940488159261892897677e
Aaron Chesley <FlashBanistan66@gmail.com>
2020-02-29 17:06:24 -0700

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { CoreModule } from './core.module';
 import { AppRouter } from './app-router.module';
 import { AppComponent } from './app.component';
 import { AppWrapperComponent } from './app-wrapper/app-wrapper.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, AppWrapperComponent],
-  imports: [BrowserModule, CoreModule, AppRouter],
+  imports: [BrowserModule, CoreModule, AppRouter, BrowserAnimationsModule],
   exports: [],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
