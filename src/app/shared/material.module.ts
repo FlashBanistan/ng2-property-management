7f51191b14df51127e27a289a4bd54a2ea120113
Aaron Chesley <FlashBanistan66@gmail.com>
2020-06-07 21:17:09 -0600

    add angular material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,110 @@
+import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatTreeModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatBadgeModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatSortModule,
+    MatTableModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatButtonToggleModule,
+    MatTooltipModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatTreeModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatBadgeModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatSortModule,
+    MatTableModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatButtonToggleModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  exports: [CommonModule, FormsModule]
+  imports: [CommonModule, MaterialModule],
+  exports: [CommonModule, FormsModule, MaterialModule]
 })
 export class SharedModule {}
