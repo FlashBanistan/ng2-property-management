328c5d1752354bc70e8400c038695f4f8bbc4c00
Aaron Chesley <FlashBanistan66@gmail.com>
2020-06-09 21:09:42 -0600

    setup navigation

--- src/app/app-wrapper/app-wrapper.component.ts
+++ src/app/app-wrapper/app-wrapper.component.ts
@@ -3,11 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-wrapper',
   templateUrl: './app-wrapper.component.html',
-  styleUrls: ['./app-wrapper.component.scss']
+  styleUrls: ['./app-wrapper.component.scss'],
 })
 export class AppWrapperComponent {
-  public collapsible = true;
-  public collapsed = false;
+  collapsible = true;
+  opened = true;
+  navLinks = [
+    { title: 'Home', url: '/connect/home', icon: 'home' },
+    { title: 'Payments', url: '/connect/payments', icon: 'payment' },
+    { title: 'Maintenance', url: '/connect/maintenance', icon: 'engineering' },
+    { title: 'Documents', url: '/connect/documents', icon: 'snippet_folder' },
+    { title: 'Property', url: '/connect/property', icon: 'apartment' },
+    { title: 'Account', url: '/connect/account', icon: 'account_box' },
+    { title: 'Help', url: '/connect/help', icon: 'help_center' },
+  ];
 
   constructor() {}
 }
