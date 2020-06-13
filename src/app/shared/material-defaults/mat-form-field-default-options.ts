79da401b7bf165e3977654ab780cfcd7d93ed6ec
Aaron Chesley <FlashBanistan66@gmail.com>
2020-06-13 12:01:09 -0600

    provide default options for mat form field

---
+++ src/app/shared/material-defaults/mat-form-field-default-options.ts
@@ -0,0 +1,6 @@
+import { MatFormFieldDefaultOptions } from '@angular/material/form-field';
+
+export const matFormFieldDefaultOptions: MatFormFieldDefaultOptions = {
+  appearance: 'outline',
+  floatLabel: 'always',
+};
