d0b9bc0bd33254114b16b476e189ae6fdf4dc118
Aaron Chesley <FlashBanistan66@gmail.com>
2020-07-04 23:49:31 -0600

    apply config changes

--- projects/propertium-common/src/public-api.ts
+++ projects/propertium-common/src/public-api.ts
@@ -4,3 +4,6 @@ export * from './lib/propertium-auth/login-credentials.interface';
 
 export * from './lib/material-defaults/mat-dialog-default-options';
 export * from './lib/material-defaults/mat-form-field-default-options';
+
+export * from './lib/material-defaults/mat-dialog-default-options';
+export * from './lib/material-defaults/mat-form-field-default-options';
